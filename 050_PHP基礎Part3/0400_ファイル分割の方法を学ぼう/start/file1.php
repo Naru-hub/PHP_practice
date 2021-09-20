4547b75618df454a519b930a230647f1d10bca07
Naru-hub <1208ny0324@gmail.com>
2021-09-20 21:00:13 +0900

    add PHP3 4~6

--- 050_PHP基礎Part3/0400_ファイル分割の方法を学ぼう/start/file1.php
+++ 050_PHP基礎Part3/0400_ファイル分割の方法を学ぼう/start/file1.php
@@ -1,1 +1,8 @@
 <?php
+if (!function_exists('fn1')) {  //関数が宣言されていない場合fn1を宣言してあげる
+  function fn1()
+  {
+    echo 'fn1 is called';
+  }
+}
+$arry['num']++;
--- 050_PHP基礎Part3/0600_名前空間について学ぼう/start/index.php
+++ 050_PHP基礎Part3/0600_名前空間について学ぼう/start/index.php
@@ -1,12 +1,21 @@
 <?php
-if(!defined('TAX_RATE')) {
-    define('TAX_RATE', 0.1);
-}
-    
-function with_tax($base_price, $tax_rate = TAX_RATE) {
-    $sum_price = $base_price + ($base_price * $tax_rate);
-    $sum_price = round($sum_price);
-    return $sum_price;
-}
+
+/**
+ *名前空間
+ *
+ *コードの格納場所
+ */
+require_once 'lib.php';
+use function lib\with_tax;
+use const \lib\sub\TAX_RATE;
 $price = with_tax(1000, 0.08);
 echo $price;
+// echo TAX_RATE;
+
+function my_echo() {
+
+}
+\my_echo();
+class GlobalCls {
+
+}
