0a31c2c6c33bd0dfaa65bd966c9c403be01d8e36
Naru-hub <1208ny0324@gmail.com>
2021-09-20 16:24:29 +0900

    PHP3  1~3

--- 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
+++ 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
@@ -1,7 +1,18 @@
 <?php
-function counter($step = 1) {
-    global $num;
+$num = 0;
+counter(2);
+/**
+ * 
+ * - 関数内の処理は関数が実行されて初めて動く
+ * - 関数宣言はプログラムの実行よりも前に準備される
+ * - それ以外は上から順に実行される
+ */
+function counter($step = 1)
+{
+    global $num;    //15行目の$numが入る
     $num += $step;
     echo $num;
     return $num;
 }
+// $num = 0;
+counter(2);
--- 050_PHP基礎Part3/0300_定数を使ってみよう/start/index.php
+++ 050_PHP基礎Part3/0300_定数を使ってみよう/start/index.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * 
+ * 定数の使い方
+ * 
+ * - define, const での定義方法
+ * - constはif文や関数の中では使えない
+ * - defineではグローバルスコープに値が配置される
+ * - constは名前空間内に配置される(名前空間のレクチャーで紹介)
+ */
+
+if (!defined('TAX_RATE')) {    //定数が定義されていない時に定数を定義する
+  // const TAX_RATE = 0.1;
+  define('TAX_RATE', 0.1);
+}
+
+function with_tax($base_price, $tax_rate = TAX_RATE)
+{
+  $sum_price = $base_price + ($base_price * $tax_rate);
+  $sum_price = round($sum_price);
+  return $sum_price;
+}
+$price = With_tax(1000, 0.08);
+echo $price;
