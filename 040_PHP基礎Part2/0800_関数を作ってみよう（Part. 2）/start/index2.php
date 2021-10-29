b428be7104450a0c941fef28d72c3dbe6569df05
Naru-hub <1208ny0324@gmail.com>
2021-10-29 23:45:39 +0900

    php2 review

--- 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index2.php
+++ 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index2.php
@@ -2,11 +2,12 @@
 
 $price = 1000;
 
-function with_tax($base_price, $tax_rate) {
-    $sum_price = $base_price + ($base_price * $tax_rate);
-    $sum_price = round($sum_price);
-    return $sum_price;
+function with_tax($base_price, $tax_rate = 0.1)    //0.1はデフォルト引数
+{
+  $sum_price = $base_price + ($base_price * $tax_rate);
+  $sum_price = round($sum_price);
+  return $sum_price;
 }
 
-$price = with_tax($price, 0.1);
+$price = with_tax($price, 0.08);
 echo $price;
